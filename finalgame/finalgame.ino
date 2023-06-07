35bda12d71e5094999829e9cbe239c0d25f3bb18
narayanvarsha <135365202+narayanvarsha@users.noreply.github.com>
2023-06-06 17:02:34 -0700

    Update finalgame.ino

--- finalgame/finalgame.ino
+++ finalgame/finalgame.ino
@@ -1,62 +1,60 @@
 #include <Adafruit_CircuitPlayground.h>
 #include <AsyncDelay.h>
-int right=5;
-int left=4;
-int level=1;
-int length=1;
+
+#define BUTTON_A A7
+#define BUTTON_B A4
+int LEVEL_DELAY=1000;
+
+int currentLevel = 1;
+int sequenceLength = 1;
 int sequence[10];
-int playerindex=0;
+int playerIndex = 0;
 
-void setup()
-{
-  Serial.begin(9600);
+void setup() {
   CircuitPlayground.begin();
+  randomSeed(analogRead(0));
 }
 
-void loop()
-{
-  if(level>10)
-  {
+void loop() {
+  if (currentLevel > 10) {
+    // Game completed
     CircuitPlayground.clearPixels();
-    CircuitPlayground.playTone(440,1000);
+    CircuitPlayground.playTone(440, 1000); // Play a sound to indicate game completion
     delay(2000);
-    restart();
+    resetGame();
   }
-  if (playerindex==length)
-  {
-    playerindex=0;
-    delay(2000);
-    nextlevel();
-  }
-  check();
-}
-void nextlevel()
-{
-  length++;
-  lights();
 
-}
-void buttonpart(int color)
-{
-  if(sequence[playerindex]==color)
-  {
-    CircuitPlayground.playTone(880,100);
-    CircuitPlayground.setPixelColor(playerindex, color == 0 ? 255 : 0, 0, color == 0 ? 255 : 0); // Light up the corresponding LED
-    playerindex++;
+  if (playerIndex == sequenceLength) {
+    // Player completed the sequence for the current level
+    playerIndex = 0;
+    delay(LEVEL_DELAY);
+    nextLevel();
   }
-  else
-  {
-    // Incorrect button pressed
-    CircuitPlayground.playTone(220, 500); // Play a sound to indicate wrong answer
-    restart();    
+
+  checkButtons();
+}
+
+void resetGame() {
+  currentLevel = 1;
+  sequenceLength = 1;
+  playerIndex = 0;
+}
+
+void nextLevel() {
+  sequenceLength++;
+  generateSequence();
+  playSequence();
+}
+
+void generateSequence() {
+  for (int i = 0; i < sequenceLength; i++) {
+    sequence[i] = random(2); // Generate random sequence of 0s (pink) and 1s (blue)
   }
 }
-void lights()
-{
-   CircuitPlayground.clearPixels();
-    for(int i=1;i<=length;i++)
-    {
-        if (sequence[i] == 0) {
+
+void playSequence() {
+  for (int i = 0; i < sequenceLength; i++) {
+    if (sequence[i] == 0) {
       CircuitPlayground.setPixelColor(i, 255, 0, 255); // Pink
     } else {
       CircuitPlayground.setPixelColor(i, 0, 0, 255); // Blue
@@ -66,17 +64,24 @@ void lights()
     delay(100);
   }
 }
-void check()
-{
-    if (CircuitPlayground.leftButton()) {
+
+void checkButtons() {
+  if (CircuitPlayground.leftButton()) {
     handleButtonPress(0); // Pink button
   } else if (CircuitPlayground.rightButton()) {
     handleButtonPress(1); // Blue button
   }
 }
-void restart()
-{
-  level=1;
-  length=1;
-  playerindex=0;
+
+void handleButtonPress(int color) {
+  if (sequence[playerIndex] == color) {
+    // Correct button pressed
+    CircuitPlayground.playTone(880, 100); // Play a sound to indicate correct answer
+    CircuitPlayground.setPixelColor(playerIndex, color == 0 ? 255 : 0, 0, color == 0 ? 255 : 0); // Light up the corresponding LED
+    playerIndex++;
+  } else {
+    // Incorrect button pressed
+    CircuitPlayground.playTone(220, 500); // Play a sound to indicate wrong answer
+    resetGame();
+  }
 }
