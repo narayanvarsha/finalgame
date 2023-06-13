ebfc6a5a714afe59292ee1986d00c6bc6521ebc0
narayanvarsha <135365202+narayanvarsha@users.noreply.github.com>
2023-06-13 14:31:00 -0700

    Update finalgame.ino

--- finalgame/finalgame.ino
+++ finalgame/finalgame.ino
@@ -1,87 +1,96 @@
 #include <Adafruit_CircuitPlayground.h>
 #include <AsyncDelay.h>
+int rightpin=5;
+int leftpin=4;
+int level=1;
+int sequence[11];
+int playerindex=1;
+int pink=1;
+int blue=0;
 
-#define BUTTON_A A7
-#define BUTTON_B A4
-int LEVEL_DELAY=1000;
 
-int currentLevel = 1;
-int sequenceLength = 1;
-int sequence[10];
-int playerIndex = 0;
-
-void setup() {
+void setup()
+{
+  Serial.begin(9600);
   CircuitPlayground.begin();
-  randomSeed(analogRead(0));
+  attachInterrupt(digitalPinToInterrupt(leftpin),leftpart,FALLING); //attaching interrupts
+  attachInterrupt(digitalPinToInterrupt(rightpin),rightpart,FALLING);
 }
 
-void loop() {
-  if (currentLevel > 10) {
-    // Game completed
+void loop()
+{
+  if(level>=10)
+  {
     CircuitPlayground.clearPixels();
-    CircuitPlayground.playTone(440, 1000); // Play a sound to indicate game completion
+    CircuitPlayground.playTone(440,1000); //indicates the game is complete
     delay(2000);
-    resetGame();
+    restartgame();
   }
-
-  if (playerIndex == sequenceLength) {
-    // Player completed the sequence for the current level
-    playerIndex = 0;
-    delay(LEVEL_DELAY);
-    nextLevel();
-  }
-
-  checkButtons();
-}
-
-void resetGame() {
-  currentLevel = 1;
-  sequenceLength = 1;
-  playerIndex = 0;
-}
-
-void nextLevel() {
-  sequenceLength++;
-  generateSequence();
-  playSequence();
-}
-
-void generateSequence() {
-  for (int i = 0; i < sequenceLength; i++) {
-    sequence[i] = random(2); // Generate random sequence of 0s (pink) and 1s (blue)
+  if (playerindex==level) //makes sure the player finished the current level amd can move on
+  {
+    playerindex=1;
+    delay(1000);
+    nextlevel();
   }
 }
 
-void playSequence() {
-  for (int i = 0; i < sequenceLength; i++) {
-    if (sequence[i] == 0) {
-      CircuitPlayground.setPixelColor(i, 255, 0, 255); // Pink
-    } else {
-      CircuitPlayground.setPixelColor(i, 0, 0, 255); // Blue
-    }
+
+void nextlevel()
+{
+  display();
+  Serial.print("Level "); //changes to the next level 
+  Serial.println(level);
+  level++;
+}
+void buttonpart(int color)
+{
+  if(sequence[playerindex]==color)
+  {
+    //Correct button pressed
+    CircuitPlayground.playTone(880,100); //play sound to indicate correct answer
+    playerindex++;
+  }
+  else
+  {
+    // Incorrect button pressed
+    CircuitPlayground.playTone(220, 500); // Play a sound to indicate wrong answer
+    restartgame();    
+  }
+}
+void display() 
+{
+  //function to play the random lights for the game
+    for(int i=1;i<=level;i++)
+    {
+      int LED_num=random(10);
+      if(LED_num%2==0)
+      {
+        CircuitPlayground.setPixelColor(LED_num,255,0,255); //pink
+        sequence[i]=pink;
+      }
+      else
+      {
+        CircuitPlayground.setPixelColor(LED_num,0,0,255); //blue
+        sequence[i]=blue;
+      }
     delay(500);
     CircuitPlayground.clearPixels();
     delay(100);
-  }
+    }
+    CircuitPlayground.clearPixels();
 }
 
-void checkButtons() {
-  if (CircuitPlayground.leftButton()) {
-    handleButtonPress(0); // Pink button
-  } else if (CircuitPlayground.rightButton()) {
-    handleButtonPress(1); // Blue button
-  }
-}
 
-void handleButtonPress(int color) {
-  if (sequence[playerIndex] == color) {
-    // Correct button pressed
-    CircuitPlayground.playTone(880, 100); // Play a sound to indicate correct answer
-    CircuitPlayground.setPixelColor(playerIndex, color == 0 ? 255 : 0, 0, color == 0 ? 255 : 0); // Light up the corresponding LED
-    playerIndex++;
-  } else {
-    // Incorrect button pressed
-    CircuitPlayground.playTone(220, 500); // Play a sound to indicate wrong answer
-    resetGame();
-  }
+void restartgame()
+{
+  level=1; //this function runs when the player eiter finishes the game or looses a level.
+  playerindex=1;
+}
+void leftpart()
+{
+    buttonpart(pink); //left button interrupt
+}
+void rightpart()
+{
+    buttonpart(blue); //right button interrupt
 }
